a8975b29debdc088aaecf8e2f06e57082b05f044
Ogi62 <danguba962@gmail.com>
2022-06-28 16:38:41 +0200

    added products-list component

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products.component';
-
+import { ProductsListComponent } from '../components/products-list/products-list.component';
 
 @NgModule({
   declarations: [
-    ProductsComponent
+    ProductsComponent,
+    ProductsListComponent
   ],
   imports: [
     CommonModule,
