3bca199b1366ae6b071ca83c19285b70249c88b9
Ognjen Dangubic <74620644+ogi62@users.noreply.github.com>
2022-06-29 12:32:21 +0200

    made task 5.mock data service (#5)

--- src/app/models/Product.ts
+++ src/app/models/Product.ts
@@ -1,12 +1,12 @@
-export class Task  {
+export class Product  {
     id: number
     title: string
     description: string
     price: number
     image: string
-    quantity: string
+    quantity: number
 
-    constructor (id: number, title: string, description: string,price: number, image: string, quantity: string) {
+    constructor (id: number, title: string, description: string,price: number, image: string, quantity: number) {
         this.id = id;
         this.title = title;
         this.description = description;
