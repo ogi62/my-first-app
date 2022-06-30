3fdf93cc818a96f9dbf3eb0d4aea1708b4b49639
Ogi62 <danguba962@gmail.com>
2022-06-30 12:03:36 +0200

    made input and made sccs for input

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -4,11 +4,14 @@ import { CommonModule } from '@angular/common';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products.component';
 import { ProductsListComponent } from '../components/products-list/products-list.component';
+import { FilterPipe } from '../pipes/filter.pipe';
+
 
 @NgModule({
   declarations: [
     ProductsComponent,
-    ProductsListComponent
+    ProductsListComponent,
+    FilterPipe,
   ],
   imports: [
     CommonModule,
