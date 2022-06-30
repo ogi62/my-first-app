6b19826a596e34279ceacef22fe47f39344dcdda
Ogi62 <danguba962@gmail.com>
2022-06-30 14:24:05 +0200

    changed details in code ... to lowecase

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -1,15 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Product } from '../models/Product';
 
 @Pipe({
   name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(Products: Array<any>, search: string): any {
+  transform(Products: Product[], search: string): Product[] {
     console.log(Products, search)
 
     if(Products && search) {
-      return Products.filter((d)=> d.title.indexOf(search) > -1)
+      return Products.filter((d)=> d.title.toLowerCase().indexOf(search.toLowerCase()) > -1)
     }
     return Products;
   }
