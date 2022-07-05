acf417961cfb9cce630af32dba02d0f20948bea8
Ogi62 <danguba962@gmail.com>
2022-07-05 16:54:04 +0200

    made edit component and edit funcionality

--- src/app/components/all-products/all-products.component.ts
+++ src/app/components/all-products/all-products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Product } from 'src/app/models/Product';
 import { ProductsService } from 'src/app/services/products.service';
 
@@ -11,14 +12,20 @@ export class AllProductsComponent implements OnInit {
 
   products: Product[] | any;
 
-  constructor( private productService: ProductsService) { }
+  constructor( private productService: ProductsService, private router: Router) { }
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe((data) => {
         this.products = data.map(p => {
+          // znam da ga dohvatim ne znam da ga promenim
+          console.log(p.payload.doc.id)
           return p.payload.doc.data()
         })
     })
+  }
+
+  editProduct(item:Product) {
+    this.router.navigate(['/admin/product/',item.id],{state: {data: {item}}});
   }
 
 }
