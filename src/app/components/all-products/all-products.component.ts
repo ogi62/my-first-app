9e334abb8cf2fa49093d58a4ef56429df1fed3ee
Ogi62 <danguba962@gmail.com>
2022-07-06 21:47:10 +0200

    14. added notifications for pro. add pro delete pro update and go back

--- src/app/components/all-products/all-products.component.ts
+++ src/app/components/all-products/all-products.component.ts
@@ -1,35 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgToastService } from 'ng-angular-popup';
 import { Product } from 'src/app/models/Product';
 import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-all-products',
   templateUrl: './all-products.component.html',
-  styleUrls: ['./all-products.component.scss']
+  styleUrls: ['./all-products.component.scss'],
 })
 export class AllProductsComponent implements OnInit {
-
   products: Product[] | any;
 
-  constructor( private productService: ProductsService, private router: Router) { }
+  constructor(
+    private productService: ProductsService,
+    private router: Router,
+    private toast: NgToastService
+  ) {}
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe((data) => {
-        this.products = data.map(p => {
-          // znam da ga dohvatim ne znam da ga promenim
-          // console.log(p.payload.doc.id);
-          return p.payload.doc.data()
-        })
-    })
+      this.products = data.map((p) => {
+        // znam da ga dohvatim ne znam da ga promenim
+        // console.log(p.payload.doc.id);
+        return p.payload.doc.data();
+      });
+    });
   }
 
-  editProduct(item:Product) {
-    this.router.navigate(['/admin/product/',item.id],{state: {data: {item}}});
+  editProduct(item: Product) {
+    this.router.navigate(['/admin/product/', item.id], {
+      state: { data: { item } },
+    });
   }
 
   deleteProduct(id: any) {
+    this.toast.error({
+      detail: 'Product Deleted',
+      summary: 'You successfully deleted the product',
+      duration: 5000,
+    });
     this.productService.deleteProduct(id);
   }
-
 }
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgToastService } from 'ng-angular-popup';
 import { Product } from 'src/app/models/Product';
 import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent implements OnInit {
-
   product!: Product;
   productTitle!: string;
   productDescription!: string;
@@ -17,34 +17,45 @@ export class ProductComponent implements OnInit {
   productPrice!: string;
   productQuantity!: string;
 
-  constructor(public fireservice: ProductsService, private router: Router) { }
+  constructor(
+    public fireservice: ProductsService,
+    private router: Router,
+    private toast: NgToastService
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   addProduct() {
-    this.product  ={
-      id: Math.floor((Math.random() * 1000000) + 1),
+    this.product = {
+      id: Math.floor(Math.random() * 1000000 + 1),
       title: this.productTitle,
       price: +this.productPrice,
       description: this.productDescription,
       quantity: +this.productQuantity,
-      image: this.productImage
-      
+      image: this.productImage,
     };
 
-    this.fireservice.addProduct(this.product).then(res => {
-
-        this.productTitle = "";
+    this.fireservice
+      .addProduct(this.product)
+      .then((res) => {
+        this.productTitle = '';
         this.productDescription = '';
-        this.productImage = "";
-        this.productPrice = "";
-        this.productQuantity = "";
-        this.router.navigate(["/admin/products"]);
-    }).catch(error => {
-      console.log(error);
-    });
-    
+        this.productImage = '';
+        this.productPrice = '';
+        this.productQuantity = '';
+        this.router.navigate(['/admin/products']);
+        this.toast.success({
+          detail: 'Product Added',
+          summary: 'You successfully added a new product !',
+          duration: 5000,
+        });
+      })
+      .catch((error) => {
+        this.toast.error({
+          detail: "Product isn't added",
+          summary: error.message,
+          duration: 5000,
+        });
+      });
   }
-
 }
