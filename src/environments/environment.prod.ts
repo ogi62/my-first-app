5f1027928bc3a8ef4dff88e3dfca6135d63d6c0d
Ogi62 <danguba962@gmail.com>
2022-07-08 17:09:39 +0200

    deploy the app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAaNXTF36Ut0l5jovyXXMLHItbQJe-g9-Q",
+    authDomain: "my-first-app-fc9f2.firebaseapp.com",
+    databaseURL: "https://my-first-app-fc9f2-default-rtdb.firebaseio.com",
+    projectId: "my-first-app-fc9f2",
+    storageBucket: "my-first-app-fc9f2.appspot.com",
+    messagingSenderId: "952086685665",
+    appId: "1:952086685665:web:61da93d5a66f59a78e94a0",
+    measurementId: "G-518ZJ04DXW"
+  }
 };
