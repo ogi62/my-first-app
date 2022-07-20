a4c2895fa13066a485178fb8d4e69e837b355aa7
Ognjen Dangubic <74620644+ogi62@users.noreply.github.com>
2022-07-20 14:05:11 +0200

    Fix routing (#24)
    * made changes to routes ... lazy loading implement correcty ... add admin paths
    
    * made lazy loading for products and changed some things for admin that i didnt do well

--- src/app/feature/products/products-routing.module.ts
+++ src/app/feature/products/products-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
+import { canActivate } from '@angular/fire/auth-guard';
 import { RouterModule, Routes } from '@angular/router';
+import { UserGuard } from 'src/app/core/guards/UserGuard/user.guard';
+import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
+import { OrdersComponent } from './orders/orders.component';
 import { ProductsComponent } from './products.component';
 
-const routes: Routes = [{ path: '', component: ProductsComponent }];
+const routes: Routes = [
+  { 
+  path: '', 
+  component: ProductsComponent 
+  },
+  {
+    path: 'order-now',
+    component: DynamicFormComponent,
+    canActivate:[UserGuard]
+  },
+  {
+    path: 'app-orders',
+    component: OrdersComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/shared/shell/header/header.component.ts
+++ src/app/shared/shell/header/header.component.ts
@@ -18,7 +18,7 @@ export class HeaderComponent implements OnInit {
 
   logout() {
     this.authService.logout().subscribe(()=> {
-      this.router.navigate(['']);
+      this.router.navigate(['/login']);
     });
   }
 
