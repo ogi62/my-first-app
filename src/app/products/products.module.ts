5fa1c4b5731d6ec2dcc7a9354a2ed929b169f15f
Ogi62 <danguba962@gmail.com>
2022-07-20 10:43:38 +0200

    moved pipes

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products.component';
 import { ProductsListComponent } from '../components/products-list/products-list.component';
-import { FilterPipe } from '../pipes/filter.pipe';
+import { FilterPipe } from '../shared/pipes/filterPipe/filter.pipe';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ModalComponent } from '../components/modal/modal.component';
 import { HttpClientModule } from '@angular/common/http';
